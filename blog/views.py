f9835cca46f3951b6f43d9c621ff3959ca355e4c
luis felipe <lfelipe.desarrollador@gmail.com>
2021-06-21 13:07:10 -0500

    se agregó sistema de etiquetas
    Ahora se puede filtrar un post también por sus etiquetas

--- blog/views.py
+++ blog/views.py
@@ -3,35 +3,45 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
+
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
+
+from taggit.models import Tag
 
 # Create your views here.
 
 
-class PostListView(ListView):
-    queryset = Post.objects.filter(status='published')
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.objects.filter(status='published')
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     object_list = Post.objects.filter(status='published')
-#     paginator = Paginator(object_list, 3) #3 post por página
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         #si la página no es un integer reparte la primera página
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # si la página está fuera de rango, reparte la última página
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {
-#         'page': page,
-#         'posts': posts
-#     })
+def post_list(request, tag_slug=None):
+    object_list = Post.objects.filter(status='published')
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3) #3 post por página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #si la página no es un integer reparte la primera página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # si la página está fuera de rango, reparte la última página
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {
+        'page': page,
+        'posts': posts,
+        'tag': tag
+    })
 
 def post_detail(request, year, month, day, posti):
     post = get_object_or_404(Post, slug=posti,
