b7266e614c20bbe710f1e5f82617d7823006d9d9
luis felipe <lfelipe.desarrollador@gmail.com>
2021-06-21 13:40:16 -0500

    se agrego recomendaciones con base en etiquetas

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
@@ -67,11 +68,17 @@ def post_detail(request, year, month, day, posti):
     else:
         comment_form = CommentForm()
 
+    #Lista de posts similares
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(status='published', tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', 'publish')[:2]
+
     return render(request,'blog/post/detail.html',{
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
         })
 
 
