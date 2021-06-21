4bef2c12fdc15532ce25edece8a5d1048dcb7149
luis felipe <lfelipe.desarrollador@gmail.com>
2021-06-21 11:43:28 -0500

    se agregó la funcionalidad de mandar correos desde el blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+
+from .forms import EmailPostForm
+from .models import Post
+
 # Create your views here.
 
 
@@ -30,9 +34,38 @@ class PostListView(ListView):
 #     })
 
 def post_detail(request, year, month, day, posti):
-    post = get_object_or_404(Post, slug=post,
+    post = get_object_or_404(Post, slug=posti,
                                     status='published',
                                     publish__year=year,
                                     publish__month=month,
                                     publish__day=day)
     return render(request,'blog/post/detail.html',{'post': post})
+
+
+def post_share(request, post_id):
+    #recupera el post por el id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+
+    if request.method == 'POST':
+        #el form fue recibido
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # el form pasó las validaciones
+            cd  = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} te recomienda que leas {post.title}"
+            message = f"Leer {post.title} en {post_url}\n\n" \
+                       f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'daluz0221@gmail.com', [cd['to']])
+            sent = True
+            #...se envía el email
+    else:
+        form = EmailPostForm()
+        
+    return render(request, 'blog/post/share.html', {
+            'post':post, 
+            'form': form,
+            'sent':sent
+            }) 
