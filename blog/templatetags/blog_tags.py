f3a7545984dcf12b7d04511622ef591c0ca8bc45
luis felipe <lfelipe.desarrollador@gmail.com>
2021-06-21 19:20:56 -0500

    cusmtom tags
    se agregaron 3 tags personalizados, que permiten mostrar los post mas comentados, ultimos publicados y cuantos hay en total

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+
+register = template.Library()
+
+@register.simple_tag
+def total_posts():
+    return Post.objects.filter(status='published').count()
+
+@register.simple_tag
+def get_most_coment_post(count=5):
+    return Post.objects.filter(status='published').annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.filter(status="published").order_by('-publish')[:count]
+    return {'l_post': latest_posts}
