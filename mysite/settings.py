0c61dbf6a2db7e43eb9a09f93722770ad5b4122a
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-03-03 00:10:53 -0500

    test 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 #cambuio para probar
 #more cositas
+#aun mas pruebas
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
